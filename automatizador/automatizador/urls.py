e24b703f1a61218a085df52ed7f5c9edbbe0e64d
fsdrasfragoso <esdrasfragoso@gmail.com>
2022-03-20 16:59:36 -0300

    [urls.py]

--- automatizador/automatizador/urls.py
+++ automatizador/automatizador/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('tarefas.urls')),
+    path('ta/', include('tarefas.urls')),
     path('admin/', admin.site.urls),
 ]
