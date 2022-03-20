bc87c5c4516623d19dcf4bcc5f3a6601fcd4c0c3
fsdrasfragoso <esdrasfragoso@gmail.com>
2022-03-20 18:04:21 -0300

    [settigs.py]

--- automatizador/automatizador/urls.py
+++ automatizador/automatizador/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('ta/', include('tarefas.urls')),
+    path('', include('tarefas.urls')),
     path('admin/', admin.site.urls),
 ]
