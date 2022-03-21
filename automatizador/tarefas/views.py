c1d662f0e46114bf7742f4fe39aa08f5a8f9de3d
fsdrasfragoso <esdrasfragoso@gmail.com>
2022-03-21 15:25:52 -0300

    bootstrap

--- automatizador/tarefas/views.py
+++ automatizador/tarefas/views.py
@@ -1,5 +1,8 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import TaskGroups 
 
 
 def index(request):
@@ -7,3 +10,10 @@ def index(request):
 
 def home(request):
     return render(request,'home.html')
+
+def task_groups(request):
+    taskGroups = TaskGroups.objects.all()
+    context = {
+        'taskGroups':taskGroups
+    }
+    return render(request,'taskGroups.html',context)
