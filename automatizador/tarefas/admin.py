c3f13988b0e0b60a506c50abc6c4a4ffbc15da73
fsdrasfragoso <esdrasfragoso@gmail.com>
2022-05-10 00:03:06 -0300

    log

--- automatizador/tarefas/admin.py
+++ automatizador/tarefas/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 
 from .models import TaskGroups, Tasks, LogTasks
 
-admin.site.register(TaskGroups)
-admin.site.register(Tasks)
-admin.site.register(LogTasks)
+class TaskGroupsAdmin(admin.ModelAdmin):
+    list_display = ("id","name","creation_date", "update_date")
+
+class TasksAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "status", "previous_status", "task_group", "creation_date", "update_date")
+    
+class LogTasksAdmin(admin.ModelAdmin):
+    list_display = ("id", "task", "status", "previous_status", "creation_date", "update_date")
+
+admin.site.register(TaskGroups, TaskGroupsAdmin)
+admin.site.register(Tasks, TasksAdmin)
+admin.site.register(LogTasks,LogTasksAdmin)
